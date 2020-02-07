cd4808e856e6b7d99bf01cc0c599daa677f806e6
Seth Donaldson <sethd63@gmail.com>
2020-02-07 09:40:51 -0700

    comment existing code

--- build_dataset.py
+++ build_dataset.py
@@ -16,12 +16,9 @@ import matplotlib.image as mpimg
 def add_ar_tag_to_img(img):
     h = img.shape[0]
     w = img.shape[1]
-    max_sz = min(h, w)*0.4
+    max_sz = min(h, w)*0.35
     id = random.randint(0, 999)
-    mult = random.randint(1, int(max_sz/7))
-
-    # i need to put the image outside the frame
-    # think of ar tag in terms of center
+    mult = random.randint(1, int(max_sz/8))
 
     # create a random AR tag
     ar = cv2.aruco.drawMarker(aruco.Dictionary_get(aruco.DICT_5X5_1000), id, sidePixels=8*mult, img=img, borderBits=1)
@@ -32,30 +29,24 @@ def add_ar_tag_to_img(img):
     for i in range(3):
         ar_0[:, :, i] = ar
 
+    # choose random center coordinate to place ar tage
     c_x = random.randint(0,w)
     c_y = random.randint(0,h)
-    # c_x = 100
-    # c_y = 1
-    print(ar.shape)
-    print(mult)
-    print(c_x, c_y)
 
+    # Bounds on where AR tag will go in src img
     min_y = int(max(0, c_y-(ar.shape[0]/2)))
     min_x = int(max(0, c_x-(ar.shape[1]/2)))
     max_y = int(min(h, c_y+(ar.shape[0]/2)))
     max_x = int(min(w, c_x+(ar.shape[1]/2)))
 
-    # There is a problem with the integer division, in some cases the stuff is too large, in others too big
+    # Bounds on AR tag (i.e. how to cut the tag when it goes off the screen)
     min_y_ar = int(np.absolute(min(0,c_y-(ar.shape[0]/2))))
     min_x_ar = int(np.absolute(min(0,c_x-(ar.shape[1]/2))))
     max_y_ar = int(min(ar.shape[0],ar.shape[0]+(h-(c_y+(ar.shape[0]/2)))))
     max_x_ar = int(min(ar.shape[1],ar.shape[1]+(w-(c_x+(ar.shape[1]/2)))))
 
-
+    # put AR tag in src img
     selection = ar_0[min_y_ar:max_y_ar, min_x_ar:max_x_ar]
-    # plt.imshow(selection)
-    # plt.show()
-
     img[min_y:max_y, min_x:max_x] = selection
 
     # Return ar's: presence (0|1), h, w, center (x,y)
@@ -67,10 +58,12 @@ def main():
     # pull in csv and read first few lines to figure out wtf you're supposed to do
     imgs = pd.read_csv("imgList.csv")
     img_name = imgs.loc[24][0]
+    utah_desert = 'utah_desert'
+    places = 'testSet_resize'
+    img_name = "utah_desert_1.jpg"
 
 
-    # no longer necessary
-    img = cv2.imread('../testSet_resize/'+img_name,)
+    img = cv2.imread('../{}/{}'.format(utah_desert, img_name),)
     img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
     img = add_ar_tag_to_img(img)
